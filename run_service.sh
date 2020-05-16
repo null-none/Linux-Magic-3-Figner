f827f61ea7a1da9157c045c63a893c896d7b670b
Dmitry Kalinin <dmitry.kalinin.email@gmail.com>
2020-05-16 09:00:24 +0300

    Update run_service.sh

--- run_service.sh
+++ run_service.sh
@@ -1,6 +1,7 @@
-	sudo apt-get install dkms
-    cd ./linux/drivers/hid
-    make clean
-    make
-    sudo rmmod hid-magicmouse
-    sudo insmod ./hid-magicmouse.ko middle_click_3finger=1
+#sudo bluetoothctl connect {94:F6:D6:B6:0E:B3} bluetooth device
+sudo apt-get install dkms
+cd ./linux/drivers/hid
+make clean
+make
+sudo rmmod hid-magicmouse
+sudo insmod ./hid-magicmouse.ko middle_click_3finger=1
